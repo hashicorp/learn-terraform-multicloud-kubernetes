82f6ceb65da510cb52f445a3f0c7dc7d813f6140
ritsok <msokolova13@gmail.com>
2021-08-11 17:07:41 -0400

    Fix k8s provider version

--- counting-service/versions.tf
+++ counting-service/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.4.0"
+      version = "~> 2.1.0"
     }
   }
   required_version = ">= 0.14"
