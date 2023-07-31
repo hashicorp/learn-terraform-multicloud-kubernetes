8ef722fe4750be2da8b27c521f30076c756cb764
Brian McClain <brianmmcclain@gmail.com>
2023-07-31 13:37:22 -0400

    Update provider versions

--- consul/versions.tf
+++ consul/versions.tf
@@ -5,12 +5,12 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "~> 2.0.1"
+      version = "~> 2.10.1"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.4.0"
+      version = "~> 2.22.0"
     }
   }
   required_version = ">= 0.14"
